7986040f08484da9ca7d077e7725ae92601ef2d7
Jean-Christophe Saad-Dupuy <saad.dupuy@yahoo.fr>
2010-10-05 10:48:09 +0200

    Added comments.

--- py/simpleHttp.py
+++ py/simpleHttp.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-
+"""
+	A sigle thread web server, may be usefull to share files.
+"""
 import BaseHTTPServer, SimpleHTTPServer
 server = BaseHTTPServer.HTTPServer(('',8080),SimpleHTTPServer.SimpleHTTPRequestHandler)
 server.serve_forever()
