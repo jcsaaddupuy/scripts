e79441737c3b090183799a07546d2a297d057289
Jean-Christophe Saad-Dupuy <saad.dupuy@yahoo.fr>
2010-11-28 13:59:20 +0100

    Ajout du support du cache.

--- ade/index.php
+++ ade/index.php
@@ -28,33 +28,98 @@ $ADE2ICS="./ade2ics-v3.3.pl";
 $FAC="UJF";
 $LOGIN="voirIMA";
 $PWD="ima";
+$CACHE_DIR="cache";
 
-#echo "Up";
-#$_GET["q"]="M1";
 
+/**
+* Fonctions utiles sur les chaines de caractères.
+*/
+function startswith($hay, $needle) {
+  return substr($hay, 0, strlen($needle)) === $needle;
+}
+
+function endswith($hay, $needle) {
+  return substr($hay, -strlen($needle)) === $needle;
+}
+
+/***
+* Vérifie que le contenu du calendrier commence et se termine par des balises ICS
+* @param calendar : contenu du calendrier au format ICS
+* @return True si le parametre donné est bien formatté
+*/
+function isCompleteVCalendar($calendar){
+	return (startswith($calendar, "BEGIN:VCALENDAR") && (endswith($calendar, "END:VCALENDAR")));
+}
+/**
+* Ecrit le contenu content dans le fichier filename
+* @param filename : nom du fichier à ecrire
+* @param content : contenu à ecrire
+*/
+function writeToFile($filename, $content){
+	$f=fopen($filename, "w");
+	#echo $f;
+	for($i=0; $i<sizeof($content); $i++){
+		fwrite($f, $content[$i]);
+	}
+	fclose($f);
+}
+/***
+* Lit le fichier dont le nom est donné en paramètre et le renvois
+* @param : filename: nom du fichier à lire
+* @return : le contenu du fichier
+*/
+function readFromFile($filename){
+	$f=fopen($filename, "r");
+	$content=fread($f, filesize($filename));	
+	fclose($f);
+	return $content;
+}
+
+function createFolder($folderName){
+	if(!is_dir($folderName)){
+		mkdir($folderName);
+	}
+}
+//A virer pour livraison. Permet de lancer le script avec la variable initialisée hors contexte web.
+$_GET["q"]="M2T";
 if(isset($_GET["q"])){
 	switch($_GET["q"]){
-		case "M2T" : $res="124";break;
-		case "M2D" : $res="138";break;
-		case "M1" : $res="5";break;
-		case "M1G1" : $res="22";break;
-		case "M1G2" : $res="17";break;
+		case "M2T" : $res="124";$fname="M2T";break;
+		case "M2D" : $res="138";$fname="M2G";break;
+		case "M1" : $res="5";$fname="M1";break;
+		case "M1G1" : $res="22";$fname="M1G1";break;
+		case "M1G2" : $res="17";$fname="M1G2";break;
 		//Default : M2 Miage \o/
-		default : $res="123";
+		default : $res="123";$fname="default";
 	}
+	//On gere un cache si ADE plante
+	$fname.=".ics";
 
 	exec( $ADE2ICS." -s ".$FAC." -l ".$LOGIN." -p '".$PWD."' -n ".$res,$output);
-	#echo "After EXEC";
+
+
+	/*
+		En cas d'erreur (contenu vide), on renvoie les infos contenues dans le cache.
+	*/
+	$calContent="";
+	for($i=0;$i<sizeof($output);$i++){
+		$calContent.=$output[$i];
+	}
+	
+	//A virer pour livraison, permet de tester le bon fonctionnement du cache
+	#$calContent="";
+	if(isCompleteVCalendar($calContent)){
+		createFolder($CACHE_DIR);
+		writeToFile($CACHE_DIR."/".$fname, $output);
+	}else{
+		//!\\
+		//Ici, si le cache n'a pas été crée un erreur sera levée.
+		//Ne devrais jamais se produire si le déploiement prévois de créer les fichiers cache.
+		$calContent=readFromFile($CACHE_DIR."/".$fname);
+	}
 	//On pose le mime type
 	header('Content-type: text/calendar; charset=utf-8');
-
-	/* 
-	$output est un tableau contenant chaque ligne de sortie de la console dans une entrée du tableau
-	ce qui nous force à le parcourir dans une boucle pour l'afficher
-	*/	
-	for($i=0; $i<sizeof($output); $i++){
-		echo $output[$i]."\n";
-	}
+	echo $calContent;
 }
 else{
 	echo "No ressource setted !\n";
